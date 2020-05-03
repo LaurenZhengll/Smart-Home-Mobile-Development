2eb8433bfbcaa511165082c459fd60a551261a9c
Meng Zikai <mengzika@deakin.edu.au>
2020-05-03 23:22:48 +1000

    Fixed sign up button problem and connected with firebase sever success (Leo, Rushitha, Zikai)

--- app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
+++ app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
@@ -10,20 +10,24 @@ import android.widget.Button;
 
 public class LoginScreen extends Activity {
 
-
+    private Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUp = (Button) findViewById(R.id.signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextActivity();
+            }
+        });
+
     }
 
-    public void nextActivity(View view){
-        String button_text;
-        button_text=((Button) view).getText().toString();
-        if(button_text.equals("Sign up"))
-        {
-            Intent intent = new Intent(this, SignUpScreen.class);
+    public void nextActivity(){
+        Intent intent = new Intent(this, SignUpScreen.class);
         startActivity(intent);
-        }
     }
 }
