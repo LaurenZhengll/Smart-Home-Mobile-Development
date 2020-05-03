f92cfca2ce685ad4c2c492a9bd5a88cf8807391a
Meng Zikai <mengzika@deakin.edu.au>
2020-05-03 22:58:30 +1000

    Firebase connection - sign up screen connection method (Leo, Rushitha, Zikai)

--- app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
+++ app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
@@ -1,13 +1,29 @@
 package com.stevecrossin.mindlab;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class LoginScreen extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginScreen extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void nextActivity(View view){
+        String button_text;
+        button_text=((Button) view).getText().toString();
+        if(button_text.equals("Sign up"))
+        {
+            Intent intent = new Intent(this, SignUpScreen.class);
+        startActivity(intent);
+        }
     }
 }
