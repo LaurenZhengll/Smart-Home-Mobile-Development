25c8e27415385d0195b081b047e8a0289e10e6e0
Meng Zikai <mengzika@deakin.edu.au>
2020-05-05 01:20:08 +1000

    test commit

--- app/src/main/java/com/stevecrossin/mindlab/SignUpScreen.java
+++ app/src/main/java/com/stevecrossin/mindlab/SignUpScreen.java
@@ -21,6 +21,7 @@ public class SignUpScreen extends AppCompatActivity {
     private EditText username;
     private EditText userPassword;
     private FirebaseAuth auth;
+    // this is for testing merge
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
