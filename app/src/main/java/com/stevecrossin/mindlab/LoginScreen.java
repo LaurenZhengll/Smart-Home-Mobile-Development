f47b6d143fc91ecccd3d1652fd3f4e99ae759bfd
Meng Zikai <mengzika@deakin.edu.au>
2020-05-06 20:31:51 +1000

    authTest

--- app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
+++ app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.text.TextUtils;
@@ -14,21 +15,49 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreen extends AppCompatActivity {
     public EditText inputEmail, inputPassword;
     private FirebaseAuth auth;
     private Button btnLogin;
 
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
+    }*/
+
+    private void updateUI (FirebaseUser user){
+        if (user != null) {
+            inputEmail.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            inputPassword.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            findViewById(R.id.signup).setVisibility(View.GONE);
+        } else {
+            inputEmail.setText(null);
+            inputPassword.setText(null);
+
+            findViewById(R.id.signup).setVisibility(View.VISIBLE);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //Get Firebase auth instance
-
-    }
-    public void tryAuth(View view) {
         auth = FirebaseAuth.getInstance();
+    }
+
+    public void onStop() {
+        super.onStop();
+        auth.getInstance().signOut();
+    }
+
+    public void tryAuth(View view) {
         if (auth.getCurrentUser() != null) {
             startActivity(new Intent(LoginScreen.this, MainActivity.class));
             finish();
@@ -43,7 +72,7 @@ public class LoginScreen extends AppCompatActivity {
         String email = inputEmail.getText().toString();
         final String password = inputPassword.getText().toString();
 
-        if (TextUtils.isEmpty(email)) {
+/*        if (TextUtils.isEmpty(email)) {
             Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -51,25 +80,18 @@ public class LoginScreen extends AppCompatActivity {
         if (TextUtils.isEmpty(password)) {
             Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
             return;
-        }
+        }*/
         auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(LoginScreen.this, new OnCompleteListener<AuthResult>() {
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            // there was an error
-                            if (password.length() < 6) {
-                                inputPassword.setError(getString(R.string.minimum_password));
-                            } else {
-                                Toast.makeText(LoginScreen.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
-                            }
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
                         } else {
-                            Intent intent = new Intent(LoginScreen.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
+                            Toast.makeText(LoginScreen.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
                         }
                     }
                 });
