b78914e49a90fef0d1332260606aeda967c99919
STEVE CROSSIN <sacrossi@deakin.edu.au>
2020-05-11 23:54:50 +1000

    Temp push changes - still in progress.

--- app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
+++ app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
@@ -1,7 +1,10 @@
 package com.stevecrossin.mindlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void bypassLogin(View view) {
+        Intent intent = new Intent(this, SettingsScreen.class);
+        startActivity(intent);
     }
 }
