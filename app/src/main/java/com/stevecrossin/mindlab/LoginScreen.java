9a0c93486d88ca6c107d68b17b79f41bf6feb67c
Thanuj <tmperera@deakin.edu.au>
2020-05-22 02:24:30 +1000

    Home screen has been implemented for both portrait and landscape orientations.

--- app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
+++ app/src/main/java/com/stevecrossin/mindlab/LoginScreen.java
@@ -1,13 +1,29 @@
 package com.stevecrossin.mindlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
+    Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginbtn = findViewById(R.id.login);
+        loginbtn.setOnClickListener(new LoginBtnOnClick());
+    }
+
+    private class LoginBtnOnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(LoginScreen.this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
