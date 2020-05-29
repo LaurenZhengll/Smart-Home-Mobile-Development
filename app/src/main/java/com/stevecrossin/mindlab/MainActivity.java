ee4459faf0d466392ebf8375eede0b84baaeb436
Zongyu Han <hanzo@deakin.edu.au>
2020-05-29 12:11:58 +0800

    Firebase services config

--- app/src/main/java/com/stevecrossin/mindlab/MainActivity.java
+++ app/src/main/java/com/stevecrossin/mindlab/MainActivity.java
@@ -4,23 +4,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference mRootReference = firebaseDatabase.getReference();
+    private DatabaseReference mChildReference = mRootReference.child("User4");
+    private DatabaseReference userNode = firebaseDatabase.getReference("User4");
+
+    TextView heartRateValue;
+    TextView pressureValue;
+
+    private String personId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //button = findViewById(R.id.settingsButton);
+
+
+        writeNewUser("child5", "Zongyu", "abc@gmail.com", "123123", "120", "140/70");
+
+    }
+
+    public void writeNewUser(String userId, String username, String email, String password, String HeartRate,  String BloodPressure) {
+        personId = mRootReference.push().getKey();
+        User user = new User(username, email, password, HeartRate, BloodPressure);
+
+        mChildReference.child(personId).setValue(user);
+
+
 
     }
 
@@ -45,6 +75,38 @@ public class MainActivity extends AppCompatActivity {
         }
         //Why is this line of code here?
         //setContentView(R.layout.layout_carer_one);
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        mChildReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //gets all the children in the rootnode
+
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    //Getting User object from dataSnapshot
+
+                    User user = data.getValue(User.class);
+
+                    if (user.getUsername().toString().equals("Zongyu")) {
+                        heartRateValue = (TextView) findViewById(R.id.heartRateValue);
+                        pressureValue = (TextView) findViewById(R.id.pressureValue);
+                        heartRateValue.setText(user.getHeartRate().toString());
+                        pressureValue.setText(user.getBloodPressure().toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
